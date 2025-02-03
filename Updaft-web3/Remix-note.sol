48ece0649650a285864a47d377d4f99b12a4cbb5
thebabalola <t.babalolajoseph@gmail.com>
2025-02-03 12:33:46 +0100

    solifity lessons, variables, arrays & structs, functions

---
+++ Updaft-web3/Remix-note.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+ //stating our solidity version
+
+contract SimpleStorage {
+
+    // -- Basic variable type:  boolean, uint, int, address, btyes --
+    bool hasFavouriteNumber = true;  //this is a boolean variable
+    uint256 favouriteNum = 88;
+
+    int256 favourNum = -56;
+    string favornum = "string num";
+
+    address myAddress = 0x2e7e2f04285Ae3D08ec78836107715EE49E8885e;
+    bytes32 fovouriteBytes32 = "cat";   //strings and bytes are literally the same thing
+    
+
+    // Array
+    uint256[] listofFavNum; //arrays of data
+
+    // Struct
+    struct Person {
+        uint256 age;
+        string name;
+    }
+
+    Person public personPat = Person(2, "pat"); //instance of struct Person
+    Person public personJohn = Person(15, "john");
+    Person public personGift = Person({age: 9, name: "gift"});
+
+    Person[3] private numOfPeople;  //static array (i.e not more than 3 persons to be added)
+    Person[] public listOfPersons; //dynamuc array
+
+
+
+    // -- Public view variable & Functions --
+    uint256 public favouriteNumber ; //help view changes or data store into the variable, under the deployed contract section
+
+
+    function store(uint256 _favNum) public {
+        favouriteNumber = _favNum;
+    } 
+
+    //pure view
+    function retrieve() public view returns (uint256) {
+        return favouriteNumber;
+    }
+
+
+}
+
+// 0xd9145CCE52D386f254917e481eB44e9943F39138
